2769718f20ae68fb0a1e9758db944fd7dc222aed
jasper <whiteana001@warren.k12.in.us>
2024-09-12 12:50:05 -0400

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        printFactors(6);
+        printFactors(32);
+        printFactors(10);
+        printFactors(-1);
+    }
+
+    private static void printFactors(int num) {
+        if(num < 0) {
+            System.out.print("Invalid Value");
+        }
+
+        for(int i = 1; i <= num; i++){
+            if(num % i == 0){
+                System.out.print(i);
+            }
+
+        }
+
+    }
+
+    
 }
